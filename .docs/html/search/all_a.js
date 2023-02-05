12d02a59c177da82b1f61e50aed5af8963806cea
silvio3105 <104866334+silvio3105@users.noreply.github.com>
2023-02-06 00:14:11 +0100

    New doxygen.

--- .docs/html/search/all_a.js
+++ .docs/html/search/all_a.js
@@ -4,5 +4,6 @@ var searchData=
   ['pin_1',['PIN',['../group__s_b_l_e.html#gga68b62458bf9d41b4e19ca9c723dae8bba048efb7462d3f8cced82716c66926168',1,'sBLE.h']]],
   ['ping_2',['ping',['../group__s_b_l_e.html#gab5b42e4d0ace4d24b4f4644a6804e200',1,'sBLE_HM']]],
   ['pinhandler_3',['pinHandler',['../group__s_b_l_e.html#gaca89637fcd24dee3be1df07d1ab8bec2',1,'sBLE.h']]],
-  ['pinstatushandler_4',['pinStatusHandler',['../group__s_b_l_e.html#gafc088c368daf28b27fb23b690d280658',1,'sBLE.h']]]
+  ['pinstatushandler_4',['pinStatusHandler',['../group__s_b_l_e.html#gafc088c368daf28b27fb23b690d280658',1,'sBLE.h']]],
+  ['print_5',['print',['../group__s_b_l_e.html#gac519a181a4343be50aecabeadab60114',1,'sBLE_HM']]]
 ];
